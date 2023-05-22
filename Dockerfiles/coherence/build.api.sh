cec6b0a226e2fb5680fb74617dba0bff8e68cd43
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-05-21 18:10:58 -0700

    coherence: add readme and build script

---
+++ Dockerfiles/coherence/build.api.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+CURRENT_SHA=$(git rev-parse --short HEAD)
+TAG=coherence-api-"$CURRENT_SHA"
+
+docker build \
+  -t "$TAG" \
+  -f Dockerfiles/coherence/Dockerfile.api \
+  .
+
+SIZE=$(docker images --format json "$TAG" | jq .Size)
+
+echo "Size: $SIZE"
